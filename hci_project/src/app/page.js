09a1087f2d3f27350dbeb4126f07dcb245b1ea66
BadFrank <francesco98vinci@gmail.com>
2024-02-16 13:32:20 +0100

    UI

--- hci_project/src/app/page.js
+++ hci_project/src/app/page.js
@@ -1,10 +1,27 @@
 import Scatterplot from "./graphs/ScatterPlot";
 import data from "./data/data";
+import {Button} from "@nextui-org/button";
 
 export default function Home() {
-  return (
-    <div>
-      <Scatterplot data={data} width={700} height={400} />
-    </div>
-  );
+    return (
+        <>
+
+
+            <div className="flex justify-center">
+
+                <p className="text-3xl font-semibold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">Group
+                    BeViCi</p>
+            </div>
+
+            <div className="pt-3">
+                <p className="font-sans">
+                    I’m Derek, an astro-engineer based in Tattooine. I like to build X-Wings at{" "}
+                    <a className="underline decoration-sky-500">My Company, Inc</a>.
+                    Outside of work, I like to <a className="underline decoration-pink-500">watch
+                    pod-racing</a> and have <a className="underline decoration-indigo-500">light-saber</a> fights.
+                </p>
+            </div>
+
+        </>
+    );
 }
---
+++ hci_project/src/components/navbar.jsx
@@ -0,0 +1,111 @@
+"use client"
+
+import React, {useState} from "react";
+import {
+    Navbar as NextUINavbar,
+    NavbarBrand,
+    NavbarMenuToggle,
+    NavbarMenuItem,
+    NavbarMenu,
+    NavbarContent,
+    NavbarItem,
+    Link,
+    Button,
+    Image,
+    Dropdown,
+    DropdownTrigger,
+    DropdownMenu,
+    DropdownItem
+} from "@nextui-org/react";
+
+
+
+export const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const menuItems = [
+        "Stream Graph",
+        "Chord Diagram",
+        "Parallel Coordinate Plot",
+        "Line Graph",
+        "Heat map",
+        "Spiral graph",
+        "Scatter plot",
+        "Ridgeline plot",
+        "BeViCi Group Info"
+    ];
+
+    return (
+        <NextUINavbar
+            isBordered
+            isMenuOpen={isMenuOpen}
+            onMenuOpenChange={setIsMenuOpen}
+        >
+            <NavbarContent className="sm:hidden" justify="start">
+                <NavbarMenuToggle aria-label={isMenuOpen ? "Close menu" : "Open menu"}/>
+            </NavbarContent>
+
+            <NavbarContent className="sm:hidden pr-3" justify="center">
+                <NavbarBrand>
+                    <Image src="/fondazione.png" height={80} width={80}/>
+                    <p className="font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">BeViCi</p>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarBrand>
+                    <Image src="/fondazione.png" height={80} width={80}/>
+                    <p className="font-bold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">BeViCi</p>
+                </NavbarBrand>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Group Info
+                    </Link>
+                </NavbarItem>
+                <NavbarItem isActive>
+                    <Dropdown>
+                        <DropdownTrigger>
+                            <Button variant="light">
+                                Representations
+                            </Button>
+                        </DropdownTrigger>
+                        <DropdownMenu aria-label="Static Actions">
+                            {menuItems.map((item, index) => (
+                                <DropdownItem key={`${item}-${index}`}>
+                                    <Link
+                                        className="w-full"
+                                        color={
+                                            index === menuItems.length - 1 ? "warning" : "foreground"
+                                        }
+                                        href="#"
+                                        size="lg"
+                                    >
+                                        {item}
+                                    </Link>
+                                </DropdownItem>
+                            ))}
+
+                        </DropdownMenu>
+                    </Dropdown>
+                </NavbarItem>
+            </NavbarContent>
+
+            <NavbarMenu>
+                {menuItems.map((item, index) => (
+                    <NavbarMenuItem key={`${item}-${index}`}>
+                        <Link
+                            className="w-full"
+                            color={
+                                index === menuItems.length - 1 ? "warning" : "foreground"
+                            }
+                            href="#"
+                            size="lg"
+                        >
+                            {item}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </NextUINavbar>
+    );
+}
