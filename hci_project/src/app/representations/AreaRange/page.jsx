c93fd3b95d975d80a9c560fa56ffcfedae1feb9c
BadFrank <38784895+FrancescoVinci@users.noreply.github.com>
2024-05-13 10:11:46 +0200

    Update page.jsx

--- hci_project/src/app/representations/AreaRange/page.jsx
+++ hci_project/src/app/representations/AreaRange/page.jsx
@@ -43,76 +43,7 @@ const locations = [
         name: "S. Margherita",
         key: "SMarghe"
     }
-
-]
-
-const ranges = [
-        [13.7, 25.6],
-        [13.3, 21.8],
-        [11.2, 19.9],
-        [7.9, 17.3],
-        [4.9, 20.6],
-        [5.1, 16.8],
-        [9.3, 21.1],
-        [11.1, 20.5],
-        [8.9, 18.4],
-        [4.6, 23.2],
-        [11.5, 26.0],
-        [8.6, 23.4],
-        [9.8, 22.2],
-        [8.1, 18.2],
-        [5.9, 20.2],
-        [4.5, 20.2],
-        [8.9, 19.8],
-        [11.1, 22.1],
-        [7.9, 26.7],
-        [15.9, 28.6],
-        [14.9, 27.5],
-        [9.5, 26.0],
-        [11.5, 22.4],
-        [8.6, 21.1],
-        [12.9, 21.7],
-        [13.6, 20.9],
-        [9.6, 23.9],
-        [8.6, 22.7],
-        [7.5, 25.7],
-        [5.5, 24.3],
-        [10.4, 21.2]
-
-    ],
-    averages = [
-        [18.1],
-        [17.1],
-        [15.2],
-        [12.7],
-        [13.3],
-        [10.6],
-        [15.6],
-        [16.1],
-        [14.0],
-        [15.3],
-        [17.5],
-        [17.5],
-        [15.3],
-        [13.9],
-        [13.7],
-        [13.8],
-        [14.0],
-        [15.8],
-        [18.6],
-        [21.5],
-        [19.8],
-        [17.6],
-        [16.8],
-        [15.6],
-        [16.7],
-        [16.3],
-        [17.2],
-        [16.0],
-        [16.9],
-        [16.1],
-        [14.5]
-    ];
+];
 
 const Page = () => {
 
@@ -135,7 +66,6 @@ const Page = () => {
     const [avgCO2, setAvgCO2] = useState([]);
     const [rangeTemperature, setRangeTemperature] = useState([]);
     const [rangeCO2, setRangeCO2] = useState([]);
-
 
 
     useEffect(() => {
@@ -161,7 +91,7 @@ const Page = () => {
                                 dates.push(new Date(`${row[0]}-${row[1]}-${row[2]}`));
                                 avgTemperature.push(parseFloat(row[3]));
                                 avgCO2.push(parseFloat(row[4]));
-                                rangeTemperature.push([parseFloat(row[5]),parseFloat(row[6])]);
+                                rangeTemperature.push([parseFloat(row[5]), parseFloat(row[6])]);
                                 rangeCO2.push([parseInt(row[7]), parseInt(row[8])]);
                             }
                         }
@@ -207,16 +137,24 @@ const Page = () => {
         xAxis: {
             type: 'datetime',
             categories: dates.map(date => {
-                return date.getDate() + "/" + (date.getMonth()+1);
+                return date.getDate() + "/" + (date.getMonth() + 1);
             }),
 
         },
 
-        yAxis: {
-            title: {
-                text: null
+        yAxis: [
+            {
+                title: {
+                    text: "Temperature"
+                }
+            },
+            {
+                title: {
+                    text: "CO2",
+                },
+                opposite: true,
             }
-        },
+        ],
 
         tooltip: {
             crosshairs: true,
@@ -228,6 +166,7 @@ const Page = () => {
         series: [
             {
                 name: 'Temperature',
+                yAxis: 0,
                 data: avgTemperature,
                 zIndex: 1,
                 marker: {
@@ -237,18 +176,44 @@ const Page = () => {
                 }
             },
             {
-                name: 'Range',
+                name: 'Range Temp',
+                yAxis: 0,
                 data: rangeTemperature,
                 type: 'arearange',
                 lineWidth: 0.5,
                 linkedTo: ':previous',
-                color: Highcharts.getOptions().colors[0],
+                color: Highcharts.getOptions().colors[4],
                 fillOpacity: 0.3,
                 zIndex: 0,
                 marker: {
                     enabled: false
                 }
-            }
+            },
+            {
+                name: 'CO2',
+                yAxis: 1,
+                data: avgCO2,
+                zIndex: 1,
+                marker: {
+                    fillColor: 'white',
+                    lineWidth: 2,
+                    lineColor: Highcharts.getOptions().colors[5]
+                }
+            },
+            {
+                name: 'Range CO2',
+                yAxis: 1,
+                data: rangeCO2,
+                type: 'arearange',
+                lineWidth: 0.5,
+                linkedTo: ':previous',
+                color: Highcharts.getOptions().colors[1],
+                fillOpacity: 0.3,
+                zIndex: 0,
+                marker: {
+                    enabled: false
+                }
+            },
         ]
     };
 
@@ -259,12 +224,9 @@ const Page = () => {
                     <p className="text-3xl font-PlayfairDisplay mb-3 ">Area Range Plot</p>
 
                     <div className="flex flex-wrap justify-start gap-2">
-                        <Chip color="default">Default</Chip>
-                        <Chip color="primary">Primary</Chip>
-                        <Chip color="secondary">Secondary</Chip>
-                        <Chip color="success">Success</Chip>
-                        <Chip color="warning">Warning</Chip>
-                        <Chip color="danger">Danger</Chip>
+                        <Chip color="primary">Temperature</Chip>
+                        <Chip color="secondary">CO2</Chip>
+
                     </div>
 
                     <p className="font-xl font-Roboto pt-5 mb-7">
