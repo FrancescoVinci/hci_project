50a466eb9f2175d11caada8b1a7d6b1e0c4a100f
antcim <antoniocimino.c@gmail.com>
2024-05-19 10:56:52 +0200

    chord diagram

--- hci_project/src/app/representations/ChordDiagram/page.jsx
+++ hci_project/src/app/representations/ChordDiagram/page.jsx
@@ -1,9 +1,7 @@
 "use client"
 
 import Highcharts from 'highcharts'
-
 import HighchartsExporting from 'highcharts/modules/exporting'
-
 import accessibility from "highcharts/modules/accessibility"
 import sankey from "highcharts/modules/sankey"
 import dependencyWheel from "highcharts/modules/dependency-wheel"
@@ -11,6 +9,8 @@ import seriesLabel from "highcharts/modules/series-label"
 import annotations from "highcharts/modules/annotations"
 import HighchartsReact from 'highcharts-react-official'
 import {Card, CardBody, Chip, Select, SelectItem} from "@nextui-org/react";
+import {usePapaParse} from "react-papaparse";
+import {useEffect, useState} from "react";
 
 if (typeof Highcharts === 'object') {
     HighchartsExporting(Highcharts);
@@ -21,8 +21,97 @@ if (typeof Highcharts === 'object') {
     annotations(Highcharts);
 }
 
+const ranges = {
+    "low": [300, 400],
+    "med": [400, 600],
+    "high": [600, 700],
+}
+
+const files = [
+    "Briati",
+    "CaFoscari",
+    "SBasilio",
+    "SMarghe",
+    "campusS",
+    "SGiobbe",
+]
 
 const Page = () => {
+    const {readRemoteFile} = usePapaParse();
+
+    const [lowCaFoscari, setLowCaFoscari] = useState(0);
+    const [medCaFoscari, setMedCaFoscari] = useState(0);
+    const [highCaFoscari, setHighCaFoscari] = useState(0);
+    const [lowBriati, setLowBriati] = useState(0);
+    const [medBriati, setMedBriati] = useState(0);
+    const [highBriati, setHighBriati] = useState(0);
+    const [lowSMarghe, setLowSMarghe] = useState(0);
+    const [medSMarghe, setMedSMarghe] = useState(0);
+    const [highSMarghe, setHighSMarghe] = useState(0);
+    const [lowSGiobbe, setLowSGiobbe] = useState(0);
+    const [medSGiobbe, setMedSGiobbe] = useState(0);
+    const [highSGiobbe, setHighSGiobbe] = useState(0);
+    const [lowSBasilio, setLowSBasilio] = useState(0);
+    const [medSBasilio, setMedSBasilio] = useState(0);
+    const [highSBasilio, setHighSBasilio] = useState(0);
+    const [lowcampusS, setLowcampusS] = useState(0);
+    const [medcampusS, setMedcampusS] = useState(0);
+    const [highcampusS, setHighcampusS] = useState(0);
+
+    useEffect(() => {
+        files.forEach(
+            (file) => {
+                readRemoteFile(`/spiralPlot/${file}.csv`, {
+                    complete: (results) => {
+                        let low = 0;
+                        let med = 0;
+                        let high = 0;
+
+                        for (let cur_month = 1; cur_month < 11; cur_month++) {
+                            results.data.forEach((row, index) => {
+                                if (index !== 0) {
+                                    const CO2 = parseFloat(row[3]);
+                                    if (CO2 >= ranges["low"][0] && CO2 < ranges["low"][1]) {
+                                        low += 1;
+                                    } else if (CO2 >= ranges["med"][0] && CO2 < ranges["med"][1]) {
+                                        med += 1;
+                                    } else if (CO2 >= ranges["high"][0] && CO2 < ranges["high"][1]) {
+                                        high += 1;
+                                    }
+                                }
+                            });
+                        }
+
+                        if (file === "Briati") {
+                            setLowBriati(low);
+                            setMedBriati(med);
+                            setHighBriati(high);
+                        } else if (file === "CaFoscari") {
+                            setLowCaFoscari(low);
+                            setMedCaFoscari(med);
+                            setHighCaFoscari(high);
+                        } else if (file === "SBasilio") {
+                            setLowSBasilio(low);
+                            setMedSBasilio(med);
+                            setHighSBasilio(high);
+                        } else if (file === "SMarghe") {
+                            setLowSMarghe(low);
+                            setMedSMarghe(med);
+                            setHighSMarghe(high);
+                        } else if (file === "campusS") {
+                            setLowcampusS(low);
+                            setMedcampusS(med);
+                            setHighcampusS(high);
+                        } else if (file === "SGiobbe") {
+                            setLowSGiobbe(low);
+                            setMedSGiobbe(med);
+                            setHighSGiobbe(high);
+                        }
+                    },
+                });
+            }
+        );
+    }, []);
 
     const options = {
         credits: {
@@ -31,11 +120,11 @@ const Page = () => {
 
         title: {
             align: "left",
-            text: "Highcharts Dependency Wheel"
+            text: "Number of Days Per CO2 Level per Station"
         },
         subtitle: {
             align: 'left',
-            text: 'Bla bla bla bla bla bla bla bla bla bla',
+            text: 'Fixed Station Data',
         },
 
         accessibility: {
@@ -47,13 +136,24 @@ const Page = () => {
         series: [{
             keys: ['from', 'to', 'weight'],
             data: [
-                ['CaFoscari', '300-400', 1],
-                ['CampusS', '400-500', 1],
-                ['Briati', '500-600', 1],
-                ['SBasilio', '600-700', 1],
-                ['SGiobbe', '400-500', 1],
-                ['SMarghe', '300-400', 1],
-                ['SMarghe', '400-500', 1],
+                ['CaFoscari', '300-400', lowCaFoscari],
+                ['CaFoscari', '400-600', medCaFoscari - 2000],
+                ['CaFoscari', '600-700', highCaFoscari],
+                ['CampusS', '300-400', lowcampusS],
+                ['CampusS', '400-600', medcampusS - 2000],
+                ['CampusS', '600-700', highcampusS ],
+                ['Briati', '300-400', lowBriati],
+                ['Briati', '400-600', medBriati - 2000],
+                ['Briati', '600-700', highBriati],
+                ['SBasilio', '300-400', lowSBasilio],
+                ['SBasilio', '400-600', medSBasilio - 2000],
+                ['SBasilio', '600-700', highSBasilio],
+                ['SGiobbe', '300-400', lowSGiobbe],
+                ['SGiobbe', '400-600', medSGiobbe - 2000],
+                ['SGiobbe', '600-700', highSGiobbe],
+                ['SMarghe', '300-400', lowSMarghe],
+                ['SMarghe', '400-600', medSMarghe - 2000],
+                ['SMarghe', '600-700', highSMarghe],
             ],
             type: 'dependencywheel',
             name: 'CO2 Ranges',
@@ -65,9 +165,13 @@ const Page = () => {
                 textPath: {
                     enabled: true
                 },
-                distance: 10
+                distance: 10,
+                padding: 0,
+                crop: false,
+                overflow: 'allow',
+                allowOverlap: true,
             },
-            size: '95%'
+            size: '100%',
         }]
 
     };
